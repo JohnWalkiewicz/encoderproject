cca9c1b78a3779455d69ac2eec82947875343282
John Walkiewicz <walkiewiczbrenda@gmail.com>
2016-10-12 10:35:28 -0700

    Radio buttons
    Made radio buttons clickable

--- encoder.js
+++ encoder.js
@@ -1,3 +1,53 @@
 document.getElementById("inputArea").addEventListener("keyup", function(){
-  document.getElementById("textArea").innerHTML = "<p>" + document.getElementById('inputText').value + "</p>"
+  document.getElementById("textArea").innerHTML += letters[.keycode];
 });
+
+//event handler for radio buttons
+$("input[type='radio']").click(function(){
+$("input:checked").prop('checked',false);
+$(this).prop('checked',true);
+
+var letters = {
+  65:"a",
+  66:"b",
+  67:"c",
+  68:"d",
+  69:"e",
+  70:"f",
+  71:"g",
+  72:"h",
+  73:"i",
+  74:"j",
+  75:"k",
+  76:"l",
+  77:"m",
+  78:"n",
+  79:"o",
+  80:"p",
+  81:"q",
+  82:"r",
+  83:"s",
+  84:"t",
+  85:"u",
+  86:"v",
+  87:"w",
+  88:"x",
+  89:"y",
+  90:"z"
+
+}
+
+});
+
+if($("input:checked").val() == "cipher"){
+
+
+}
+
+if($("input:checked").val() == "heiroglyphics"){
+
+}
+
+if($("input:checked").val() == "runes"){
+
+}
